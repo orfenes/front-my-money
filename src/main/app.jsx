ce4acc5dd93e461f45fb54677576ca086c3df11e
Rodrigo Castro <rfocastro@sp.r7.com>
2018-04-18 19:30:36 -0300

    modificando package e inserindo novos componentes de menu e footer

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -3,10 +3,15 @@ import React  from 'react'
 
 import Header from '../commom/template/header'
 import SideBar from '../commom/template/sideBar'
+import Footer from '../commom/template/footer'
 
 export default props => (
   <div className='wrapper'>
     <Header />
     <SideBar />
+    <div className='content-wrapper'>
+      <h1>Conteudo</h1>
+    </div>
+    <Footer />
   </div>
 )
