5968152fb3bfcaa1d03e2b4fce2dbacec229052a
Rodrigo Castro <rfocastro@sp.r7.com>
2018-04-20 19:22:56 -0300

    criando compoentes de exibicao dos valores

--- src/dashboard/dashboard.jsx
+++ src/dashboard/dashboard.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 
 import ContentHeader from '../commom/template/contentHeader'
 import Content from '../commom/template/content'
+import ValueBox from '../commom/widget/valueBox'
+import Row from '../commom/layout/row'
 
 class Dashborad extends Component {
   render(){
@@ -9,7 +11,14 @@ class Dashborad extends Component {
       <div>
         <ContentHeader title='Dashboard' small='Versão 1.0' />
         <Content>
-          Dashboard
+          <Row>
+            <ValueBox cols='12 4' color='green' icon='bank'
+                      value='R$ 10' text='Total de Creditos' />
+            <ValueBox cols='12 4' color='red' icon='credit-card'
+                      value='R$ 10' text='Total de Débitos' />
+            <ValueBox cols='12 4' color='blue' icon='money'
+                      value='R$ 0' text='Valor Consolidado' />
+          </Row>
         </Content>
       </div>
     )
