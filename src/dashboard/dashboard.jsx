0126e658ab540fca63ace5682b64751c28f704d5
Rodrigo Castro <rfocastro@sp.r7.com>
2018-04-23 14:56:07 -0300

    Inserindo redux e axios nos componentes

--- src/dashboard/dashboard.jsx
+++ src/dashboard/dashboard.jsx
@@ -1,23 +1,33 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 
+import { getSummary } from './dashboradActions'
 import ContentHeader from '../commom/template/contentHeader'
 import Content from '../commom/template/content'
 import ValueBox from '../commom/widget/valueBox'
 import Row from '../commom/layout/row'
 
 class Dashborad extends Component {
+
+  componentWillMount(){
+    this.props.getSummary();
+  }
+
   render(){
+    const { credit, debt } = this.props.summary
+
     return (
       <div>
         <ContentHeader title='Dashboard' small='Versão 1.0' />
         <Content>
           <Row>
             <ValueBox cols='12 4' color='green' icon='bank'
-                      value='R$ 10' text='Total de Creditos' />
+                      value={`R$ ${credit}`} text='Total de Creditos' />
             <ValueBox cols='12 4' color='red' icon='credit-card'
-                      value='R$ 10' text='Total de Débitos' />
+                      value={`R$ ${debt}`} text='Total de Débitos' />
             <ValueBox cols='12 4' color='blue' icon='money'
-                      value='R$ 0' text='Valor Consolidado' />
+                      value={`R$ ${credit - debt}`} text='Valor Consolidado' />
           </Row>
         </Content>
       </div>
@@ -25,4 +35,7 @@ class Dashborad extends Component {
   }
 }
 
-export default Dashborad
+const mapStateToProps = state => ({ summary: state.dashboard.summary })
+const mapDispatchToProps = dispatch => bindActionCreators({getSummary}, dispatch)
+export default connect(mapStateToProps, mapDispatchToProps)(Dashborad);
+
